3dc12055a9684fcae8771cea81b221a6cc2db305
Jarod1230 <Jarod1230@users.noreply.github.com>
2026-05-01 17:04:30 +0200

    fix: add Equatable to MeshMessage, remove redundant Equatable from MeshContact, fix SNR unit

--- MeshCoreMac/Models/MeshContact.swift
+++ MeshCoreMac/Models/MeshContact.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct MeshContact: Identifiable, Hashable, Equatable, Sendable {
+struct MeshContact: Identifiable, Hashable, Sendable {
     let id: String     // Hex-Node-Adresse aus MeshCore (z.B. "a1b2c3d4")
     var name: String
     var lastSeen: Date?
